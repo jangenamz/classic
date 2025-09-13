3bf67e8d00513cdc03db6015b6eb0c7101eb5a8f
jangenamz <danantaraindonesia.org@gmail.com>
2025-09-13 16:19:48 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file plugins/themes/classic/index.php
+ *
+ * Copyright (c) 2014-2025 Simon Fraser University
+ * Copyright (c) 2003-2025 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @ingroup plugins_themes_Classic
+ * @brief Wrapper for Classic theme plugin.
+ *
+ */
+
+require_once('ClassicThemePlugin.inc.php');
+
+return new ClassicThemePlugin();
